0cfa7617e9f177f4b7b9a1b281e2c115b156cca6
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2004-03-14 13:38:50 +0000

    change filename to lowercase

---
+++ src/include/FileZilla.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "config.h"
+
+#include <wx/wx.h>
+#include <wx/splitter.h>
+#include <wx/wxhtml.h>
+#include <wx/socket.h>
+#include <wx/xml/xml.h>
+#include <wx/xrc/xmlres.h>
+#include <wx/image.h>
+
+#include "optionsbase.h"
+#include "logging.h"
+#include "server.h"
+#include "commands.h"
+#include "FileZillaEngine.h"
