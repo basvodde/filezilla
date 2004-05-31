344f105c77e7921da5c74c8ad58176224e722180
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2004-05-31 16:15:50 +0000

    missing CTransferSocket files

---
+++ src/engine/transfersocket.cpp
@@ -0,0 +1,215 @@
+#include "FileZilla.h"
+#include "transfersocket.h"
+#include "FtpControlSocket.h"
+#include "directorylistingparser.h"
+
+BEGIN_EVENT_TABLE(CTransferSocket, wxEvtHandler)
+	EVT_SOCKET(wxID_ANY, CTransferSocket::OnSocketEvent)
+END_EVENT_TABLE();
+
+CTransferSocket::CTransferSocket(CFileZillaEngine *pEngine, CFtpControlSocket *pControlSocket, enum TransferMode transferMode)
+{
+	m_pEngine = pEngine;
+	m_pControlSocket = pControlSocket;
+
+	m_pSocketClient = 0;
+	m_pSocketServer = 0;
+	m_pSocket = 0;
+
+	m_bActive = false;
+
+	SetEvtHandlerEnabled(true);
+
+	if (transferMode == list)
+		m_pDirectoryListingParser = new CDirectoryListingParser(pEngine);
+	else
+		m_pDirectoryListingParser = 0;
+
+	m_transferMode = transferMode;
+}
+
+CTransferSocket::~CTransferSocket()
+{
+	delete m_pSocketClient;
+	delete m_pSocketServer;
+	if (!m_pSocketClient && !m_pSocketServer)
+		delete m_pSocket;
+
+	delete m_pDirectoryListingParser;
+}
+
+wxString CTransferSocket::SetupActiveTransfer()
+{
+	// Void all previous attempts to createt a socket
+	if (!m_pSocketClient && !m_pSocketServer)
+	{
+		delete m_pSocket;
+		m_pSocket = 0;
+	}
+	delete m_pSocketClient;
+	m_pSocketClient = 0;
+	delete m_pSocketServer;
+	m_pSocketServer = 0;
+	
+	wxIPV4address addr, controladdr;
+	addr.AnyAddress();
+	addr.Service(0);
+
+	m_pSocketServer = new wxSocketServer(addr, wxSOCKET_NOWAIT);
+	if (!m_pSocketServer->Ok() || !m_pSocketServer->GetLocal(addr) || !m_pControlSocket->GetLocal(controladdr))
+	{
+		delete m_pSocketServer;
+		m_pSocketServer = 0;
+		return _T("");
+	}
+	wxString port = controladdr.IPAddress() + wxString::Format(_T(",%d,%d"), addr.Service() / 256, addr.Service() % 256);
+	port.Replace(_T("."), _T(","));
+
+	m_pSocketServer->SetEventHandler(*this);
+	m_pSocketServer->SetNotify(wxSOCKET_INPUT_FLAG | wxSOCKET_OUTPUT_FLAG | wxSOCKET_CONNECTION_FLAG | wxSOCKET_LOST_FLAG);
+	m_pSocketServer->Notify(true);
+
+	return port;
+}
+
+void CTransferSocket::OnSocketEvent(wxSocketEvent &event)
+{
+	switch (event.GetSocketEvent())
+	{
+	case wxSOCKET_CONNECTION:
+		OnConnect(event);
+		break;
+	case wxSOCKET_INPUT:
+		OnReceive();
+		break;
+	case wxSOCKET_OUTPUT:
+		OnSend();
+		break;
+	case wxSOCKET_LOST:
+		OnClose(event);
+		break;
+	}
+}
+
+void CTransferSocket::OnConnect(wxSocketEvent &event)
+{
+	if (m_pSocketServer)
+	{
+		wxSocketBase *pSocket = m_pSocketServer->Accept(false);
+		if (!pSocket)
+			return;
+		delete m_pSocketServer;
+		m_pSocketServer = 0;
+		m_pSocket = pSocket;
+
+		m_pSocket->SetEventHandler(*this);
+		m_pSocket->SetNotify(wxSOCKET_INPUT_FLAG | wxSOCKET_OUTPUT_FLAG | wxSOCKET_LOST_FLAG);
+		m_pSocket->Notify(true);
+	}
+}
+
+void CTransferSocket::OnReceive()
+{
+	if (!m_pSocket || !m_bActive)
+		return;
+
+	if (m_transferMode == list)
+	{
+		char *pBuffer = new char[4096];
+		m_pSocket->Read(pBuffer, 4096);
+		if (m_pSocket->Error())
+		{
+			delete [] pBuffer;
+			int error = m_pSocket->LastError();
+			if (error == wxSOCKET_NOERROR)
+				TransferEnd(0);
+			else if (error != wxSOCKET_WOULDBLOCK)
+				TransferEnd(1);
+			return;
+		}
+		int numread = m_pSocket->LastCount();
+
+		if (numread > 0)
+			m_pDirectoryListingParser->AddData(pBuffer, numread);
+		else
+		{
+			delete [] pBuffer;
+			if (!numread)
+				TransferEnd(0);
+			else if (numread < 0)
+				TransferEnd(1);
+		}
+	}
+}
+
+void CTransferSocket::OnSend()
+{
+	if (!m_bActive)
+		return;
+}
+
+void CTransferSocket::OnClose(wxSocketEvent &event)
+{
+	if (m_pSocket)
+for(int i = 0; i < 1000;i++)		OnReceive();
+	TransferEnd(0);
+}
+
+bool CTransferSocket::SetupPassiveTransfer(wxString host, int port)
+{
+	// Void all previous attempts to createt a socket
+	if (!m_pSocketClient && !m_pSocketServer)
+	{
+		delete m_pSocket;
+		m_pSocket = 0;
+	}
+	delete m_pSocketClient;
+	m_pSocketClient = 0;
+	delete m_pSocketServer;
+	m_pSocketServer = 0;
+
+	m_pSocketClient = new wxSocketClient(wxSOCKET_NOWAIT);
+
+	m_pSocketClient->SetEventHandler(*this);
+	m_pSocketClient->SetNotify(wxSOCKET_INPUT_FLAG | wxSOCKET_OUTPUT_FLAG | wxSOCKET_CONNECTION_FLAG | wxSOCKET_LOST_FLAG);
+
+	wxIPV4address addr;
+	addr.Hostname(host);
+	addr.Service(port);
+
+	bool res = m_pSocketClient->Connect(addr, false);
+
+	if (!res && m_pSocketClient->LastError() != wxSOCKET_WOULDBLOCK)
+		return false;
+
+	m_pSocket = m_pSocketClient;
+
+	return true;
+}
+
+void CTransferSocket::SetActive()
+{
+	m_bActive = true;
+	if (m_pSocket && m_pSocket->IsConnected())
+	{
+		OnReceive();
+		OnSend();
+	}
+}
+
+void CTransferSocket::TransferEnd(int reason)
+{
+	if (!m_pSocket)
+		return;
+	if (!m_pSocketClient && !m_pSocketServer)
+	{
+		delete m_pSocket;
+		m_pSocket = 0;
+	}
+	delete m_pSocketClient;
+	m_pSocketClient = 0;
+	delete m_pSocketServer;
+	m_pSocketServer = 0;
+
+	m_pEngine->SendEvent(engineTransferEnd, reason);
+}
---
+++ src/engine/transfersocket.h
@@ -0,0 +1,53 @@
+#ifndef __TRANSFERSOCKET_H__
+#define __TRANSFERSOCKET_H__
+
+class CFileZillaEngine;
+class CFtpControlSocket;
+class CDirectoryListingParser;
+
+enum TransferMode
+{
+	list,
+	upload,
+	download
+};
+
+class CTransferSocket : public wxEvtHandler
+{
+public:
+	CTransferSocket(CFileZillaEngine *pEngine, CFtpControlSocket *pControlSocket, enum TransferMode transferMode);
+	~CTransferSocket();
+
+	wxString SetupActiveTransfer();
+	bool SetupPassiveTransfer(wxString host, int port);
+
+	void SetActive();
+
+	CDirectoryListingParser *m_pDirectoryListingParser;
+
+protected:
+	void TransferEnd(int reason);
+
+	virtual void OnSocketEvent(wxSocketEvent &event);
+	virtual void OnConnect(wxSocketEvent &event);
+	virtual void OnReceive();
+	virtual void OnSend();
+	virtual void OnClose(wxSocketEvent &event);
+
+	DECLARE_EVENT_TABLE();
+
+	wxSocketBase *m_pSocket; // Will point to either client or server socket
+
+	// Only one of the following will be non-zero
+	wxSocketClient *m_pSocketClient;
+	wxSocketServer *m_pSocketServer;
+
+	CFileZillaEngine *m_pEngine;
+	CFtpControlSocket *m_pControlSocket;
+
+	bool m_bActive;
+
+	enum TransferMode m_transferMode;
+};
+
+#endif
