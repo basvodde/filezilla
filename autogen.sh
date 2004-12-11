7dbc39ba07c3a63d6e15ebd7340e74c120ed5de6
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2004-12-11 22:12:53 +0000

    minor improvements

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo -e " --- FileZilla 3 autogen script ---\n"
-echo -e "\033[1;34mHINT:\033[0m If this script fails, try \033[0;34m./update-configure.sh\033[0m\n"
+echo -e "\033[1mHINT:\033[0m If this script fails, try \033[1m./update-configure.sh\033[0m\n"
 
 failedAclocal()
 {
@@ -13,12 +13,12 @@ failedAclocal()
 	echo '    Especially if you get an error about undefined'
 	echo '    AC_PROG_LIBTOOL, search for a .m4 file with libtool in'
 	echo '    its name and copy the file to the directory,'
-	echo '    `aclocal --print-ac-dir` prints.'
+	echo -e '    \033[1maclocal --print-ac-dir\033[0m prints.'
 
 	exit 1
 }
 
-echo -en "1. Cleaning previous files... "
+echo -n "1. Cleaning previous files... "
 
 rm -rf  configure config.log aclocal.m4 \
 	config.status config autom4te.cache libtool
@@ -63,7 +63,7 @@ if test ! -f configure || test ! -f config/ltmain.sh || test ! -f Makefile.in; t
   echo -e "\nError: Unable to generate all required files!\033[0m\n"
   echo "Please make sure you have you have autoconf 2.5, automake 1.7, libtool 1.5,"
   echo "autoheader and aclocal installed."
-  echo -e "If you don't have access to these tools, you can use the \033[0;34mupdate-configure.sh\033[0m"
+  echo -e "If you don't have access to these tools, you can use the \033[1mupdate-configure.sh\033[0m"
   echo "script which will download the generated files, just remember to call it from"
   echo "time to time, it will only download if some files have been changed"
 
@@ -71,5 +71,5 @@ if test ! -f configure || test ! -f config/ltmain.sh || test ! -f Makefile.in; t
 fi
 echo done
 echo -e "\nScript completed successfully."
-echo -e "Now run \033[0;34m./configure\033[0m, see \033[0;34m./configure --help\033[0m for more information"
+echo -e "Now run \033[1m./configure\033[0m, see \033[1m./configure --help\033[0m for more information"
 
