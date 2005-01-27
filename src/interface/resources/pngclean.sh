34727bb99cd114308d7b587c0826c295b27e83ae
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2005-01-27 12:07:40 +0000

    Small script to clean .png files using pngcrush

---
+++ src/interface/resources/pngclean.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+# Script to clean .png files using pngcrush
+# Should be run after adding of modifying .png files
+
+# Settings
+PNGCRUSH=pngcrush
+PNGCRUSH_OPTIONS="-brute -reduce -rem gAMA -rem tIME -rem pHYs -rem cHRM"
+
+FILES=`find -name "*.png"`
+
+for i in $FILES; do
+  $PNGCRUSH $PNGCRUSH_OPTIONS $i $i~ | grep -v 'IDAT length with method' | grep -v '^ |' | grep -v '^$' | grep -v 'CPU time used' | grep -v 'seconds)' | grep -v ' version: ' | grep -v 'versions are different' || die
+  mv $i~ $i
+done
