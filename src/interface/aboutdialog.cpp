a9ee1037d6a8cd2a1df519188c8d3b52e79f2440
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2006-08-06 21:00:02 +0000

    Display host

--- src/interface/aboutdialog.cpp
+++ src/interface/aboutdialog.cpp
@@ -14,6 +14,23 @@ bool CAboutDialog::Create(wxWindow* parent)
 	if (!SetLabel(XRCID("ID_VERSION"), CBuildInfo::GetVersion()))
 		return false;
 
+	wxStaticText* pHost = XRCCTRL(*this, "ID_HOST", wxStaticText);
+	if (!pHost)
+		return false;
+
+	wxStaticText* pHostDesc = XRCCTRL(*this, "ID_HOST_DESC", wxStaticText);
+	if (!pHostDesc)
+		return false;
+
+	wxString host = CBuildInfo::GetHostname();
+	if (host == _T(""))
+	{
+		pHost->Hide();
+		pHostDesc->Hide();
+	}
+	else
+		pHost->SetLabel(host);
+
 	if (!SetLabel(XRCID("ID_BUILDDATE"), CBuildInfo::GetBuildDateString()))
 		return false;
 
