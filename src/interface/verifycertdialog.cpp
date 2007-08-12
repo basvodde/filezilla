d8174215ba34800bc621a520f66fbcd8d3847d94
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2007-08-12 09:46:50 +0000

    Undo debugging code changes

--- src/interface/verifycertdialog.cpp
+++ src/interface/verifycertdialog.cpp
@@ -74,7 +74,7 @@ void CVerifyCertDialog::ShowVerificationDialog(CCertificateNotification* pNotifi
 	ParseDN(pDlg, pNotification->GetIssuer(), pSizer);
 	XRCCTRL(*pDlg, "ID_ISSUER_DUMMY", wxStaticText)->Destroy();
 
-	//if (warning)
+	if (warning)
 	{
 		XRCCTRL(*pDlg, "ID_IMAGE", wxStaticBitmap)->SetBitmap(wxArtProvider::GetBitmap(wxART_WARNING));
 		XRCCTRL(*pDlg, "ID_ALWAYS", wxCheckBox)->Enable(false);
