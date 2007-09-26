834e0ca81a7ef2ae42be9a4192fa38e6b67fbd08
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2007-09-26 23:36:41 +0000

    minor cleanup

--- src/interface/fileexistsdlg.cpp
+++ src/interface/fileexistsdlg.cpp
@@ -24,7 +24,7 @@ bool CFileExistsDlg::Create(wxWindow* parent)
 	GetSizer()->Fit(this);
 	GetSizer()->SetSizeHints(this);
 	
-	return TRUE;
+	return true;
 }
 
 void CFileExistsDlg::CreateControls()
