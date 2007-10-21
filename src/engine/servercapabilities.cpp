9da2aed166b51297fa26aadbcd750035e061aab4
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2007-10-21 10:07:43 +0000

    Add timezone_offset capability.

--- src/engine/servercapabilities.cpp
+++ src/engine/servercapabilities.cpp
@@ -14,12 +14,34 @@ enum capabilities CCapabilities::GetCapability(enum capabilityNames name, wxStri
 	return iter->second.cap;
 }
 
+enum capabilities CCapabilities::GetCapability(enum capabilityNames name, int* pOption) const
+{
+	const std::map<enum capabilityNames, struct CCapabilities::t_cap>::const_iterator iter = m_capabilityMap.find(name);
+	if (iter == m_capabilityMap.end())
+		return unknown;
+
+	if (iter->second.cap == yes && pOption)
+		*pOption = iter->second.number;
+	return iter->second.cap;
+}
+
 void CCapabilities::SetCapability(enum capabilityNames name, enum capabilities cap, const wxString& option /*=_T("")*/)
 {
 	wxASSERT(cap == yes || option == _T(""));
 	struct CCapabilities::t_cap tcap;
 	tcap.cap = cap;
 	tcap.option = option;
+	tcap.number = 0;
+
+	m_capabilityMap[name] = tcap;
+}
+
+void CCapabilities::SetCapability(enum capabilityNames name, enum capabilities cap, int option)
+{
+	wxASSERT(cap == yes || option == 0);
+	struct CCapabilities::t_cap tcap;
+	tcap.cap = cap;
+	tcap.number = option;
 
 	m_capabilityMap[name] = tcap;
 }
@@ -33,7 +55,30 @@ enum capabilities CServerCapabilities::GetCapability(const CServer& server, enum
 	return iter->second.GetCapability(name, pOption);
 }
 
+enum capabilities CServerCapabilities::GetCapability(const CServer& server, enum capabilityNames name, int* pOption)
+{
+	const std::map<CServer, CCapabilities>::const_iterator iter = m_serverMap.find(server);
+	if (iter == m_serverMap.end())
+		return unknown;
+
+	return iter->second.GetCapability(name, pOption);
+}
+
 void CServerCapabilities::SetCapability(const CServer& server, enum capabilityNames name, enum capabilities cap, const wxString& option /*=_T("")*/)
+{
+	const std::map<CServer, CCapabilities>::iterator iter = m_serverMap.find(server);
+	if (iter == m_serverMap.end())
+	{
+		CCapabilities capabilities;
+		capabilities.SetCapability(name, cap, option);
+		m_serverMap[server] = capabilities;
+		return;
+	}
+
+	iter->second.SetCapability(name, cap, option);
+}
+
+void CServerCapabilities::SetCapability(const CServer& server, enum capabilityNames name, enum capabilities cap, int option)
 {
 	const std::map<CServer, CCapabilities>::iterator iter = m_serverMap.find(server);
 	if (iter == m_serverMap.end())
--- src/engine/servercapabilities.h
+++ src/engine/servercapabilities.h
@@ -23,23 +23,32 @@ enum capabilityNames
 	pret_command,
 	mdtm_command,
 	size_command,
-
 	mode_z_support,
+	list_hidden_support, // LIST -a command
 
-	list_hidden_support // LIST -a command
+	// Server timezone offset. If using FTP, LIST details are unspecified and
+	// can return different times than the UTC based times using the MLST or
+	// MDTM commands.
+	// Not that the user can invoke an additional timezone offset on top of
+	// this for server not supporting auto-detection or to compensate 
+	// unsynchronized clocks.
+	timezone_offset
 };
 
 class CCapabilities
 {
 public:
 	enum capabilities GetCapability(enum capabilityNames name, wxString* pOption = 0) const;
+	enum capabilities GetCapability(enum capabilityNames name, int* pOption) const;
 	void SetCapability(enum capabilityNames name, enum capabilities cap, const wxString& option = _T(""));
+	void SetCapability(enum capabilityNames name, enum capabilities cap, int option);
 
 protected:
 	struct t_cap
 	{
 		enum capabilities cap;
 		wxString option;
+		int number;
 	};
 	std::map<enum capabilityNames, struct t_cap> m_capabilityMap;
 };
@@ -49,8 +58,10 @@ class CServerCapabilities
 public:
 	// If return value isn't 'yes', pOptions remains unchanged
 	static enum capabilities GetCapability(const CServer& server, enum capabilityNames name, wxString* pOption = 0);
+	static enum capabilities GetCapability(const CServer& server, enum capabilityNames name, int* option);
 
 	static void SetCapability(const CServer& server, enum capabilityNames name, enum capabilities cap, const wxString& option = _T(""));
+	static void SetCapability(const CServer& server, enum capabilityNames name, enum capabilities cap, int option);
 
 protected:
 	static std::map<CServer, CCapabilities> m_serverMap;
