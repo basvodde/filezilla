643967f95c80f6b5d9affd3e06ce2a43e9cb3301
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2008-06-03 22:36:46 +0000

    Add another test

--- tests/serverpathtest.cpp
+++ tests/serverpathtest.cpp
@@ -15,6 +15,7 @@ class CServerPathTest : public CppUnit::TestFixture
 	CPPUNIT_TEST(testGetParent);
 	CPPUNIT_TEST(testFormatSubdir);
 	CPPUNIT_TEST(testGetCommonParent);
+	CPPUNIT_TEST(testFormatFilename);
 	CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -26,6 +27,7 @@ public:
 	void testGetParent();
 	void testFormatSubdir();
 	void testGetCommonParent();
+	void testFormatFilename();
 
 protected:
 };
@@ -34,213 +36,213 @@ CPPUNIT_TEST_SUITE_REGISTRATION(CServerPathTest);
 
 void CServerPathTest::testGetPath()
 {
-	CServerPath unix1(_T("/"));
+	const CServerPath unix1(_T("/"));
 	CPPUNIT_ASSERT(unix1.GetPath() == _T("/"));
 
-	CServerPath unix2(_T("/foo"));
+	const CServerPath unix2(_T("/foo"));
 	CPPUNIT_ASSERT(unix2.GetPath() == _T("/foo"));
 
-	CServerPath unix3(_T("/foo/bar"));
+	const CServerPath unix3(_T("/foo/bar"));
 	CPPUNIT_ASSERT(unix3.GetPath() == _T("/foo/bar"));
 
-	CServerPath vms1(_T("FOO:[BAR]"));
+	const CServerPath vms1(_T("FOO:[BAR]"));
 	CPPUNIT_ASSERT(vms1.GetPath() == _T("FOO:[BAR]"));
 
-	CServerPath vms2(_T("FOO:[BAR.TEST]"));
+	const CServerPath vms2(_T("FOO:[BAR.TEST]"));
 	CPPUNIT_ASSERT(vms2.GetPath() == _T("FOO:[BAR.TEST]"));
 
-	CServerPath vms3(_T("FOO:[BAR^.TEST]"));
+	const CServerPath vms3(_T("FOO:[BAR^.TEST]"));
 	CPPUNIT_ASSERT(vms3.GetPath() == _T("FOO:[BAR^.TEST]"));
 
-	CServerPath vms4(_T("FOO:[BAR^.TEST.SOMETHING]"));
+	const CServerPath vms4(_T("FOO:[BAR^.TEST.SOMETHING]"));
 	CPPUNIT_ASSERT(vms4.GetPath() == _T("FOO:[BAR^.TEST.SOMETHING]"));
 
-	CServerPath dos1(_T("C:\\"));
+	const CServerPath dos1(_T("C:\\"));
 	CPPUNIT_ASSERT(dos1.GetPath() == _T("C:\\"));
 
-	CServerPath dos2(_T("C:\\FOO"));
+	const CServerPath dos2(_T("C:\\FOO"));
 	CPPUNIT_ASSERT(dos2.GetPath() == _T("C:\\FOO"));
 
-	CServerPath mvs1(_T("'FOO'"), MVS);
+	const CServerPath mvs1(_T("'FOO'"), MVS);
 	CPPUNIT_ASSERT(mvs1.GetPath() == _T("'FOO'"));
 
-	CServerPath mvs2(_T("'FOO.'"), MVS);
+	const CServerPath mvs2(_T("'FOO.'"), MVS);
 	CPPUNIT_ASSERT(mvs2.GetPath() == _T("'FOO.'"));
 
-	CServerPath mvs3(_T("'FOO.BAR'"), MVS);
+	const CServerPath mvs3(_T("'FOO.BAR'"), MVS);
 	CPPUNIT_ASSERT(mvs3.GetPath() == _T("'FOO.BAR'"));
 
-	CServerPath mvs4(_T("'FOO.BAR.'"), MVS);
+	const CServerPath mvs4(_T("'FOO.BAR.'"), MVS);
 	CPPUNIT_ASSERT(mvs4.GetPath() == _T("'FOO.BAR.'"));
 
-	CServerPath vxworks1(_T(":foo:"));
+	const CServerPath vxworks1(_T(":foo:"));
 	CPPUNIT_ASSERT(vxworks1.GetPath() == _T(":foo:"));
 
-	CServerPath vxworks2(_T(":foo:bar"));
+	const CServerPath vxworks2(_T(":foo:bar"));
 	CPPUNIT_ASSERT(vxworks2.GetPath() == _T(":foo:bar"));
 
-	CServerPath vxworks3(_T(":foo:bar/test"));
+	const CServerPath vxworks3(_T(":foo:bar/test"));
 	CPPUNIT_ASSERT(vxworks3.GetPath() == _T(":foo:bar/test"));
 
 	// ZVM is same as Unix, only makes difference in directory
 	// listing parser.
-	CServerPath zvm1(_T("/"), ZVM);
+	const CServerPath zvm1(_T("/"), ZVM);
 	CPPUNIT_ASSERT(zvm1.GetPath() == _T("/"));
 
-	CServerPath zvm2(_T("/foo"), ZVM);
+	const CServerPath zvm2(_T("/foo"), ZVM);
 	CPPUNIT_ASSERT(zvm2.GetPath() == _T("/foo"));
 
-	CServerPath zvm3(_T("/foo/bar"), ZVM);
+	const CServerPath zvm3(_T("/foo/bar"), ZVM);
 	CPPUNIT_ASSERT(zvm3.GetPath() == _T("/foo/bar"));
 
-	CServerPath hpnonstop1(_T("\\mysys"), HPNONSTOP);
+	const CServerPath hpnonstop1(_T("\\mysys"), HPNONSTOP);
 	CPPUNIT_ASSERT(hpnonstop1.GetPath() == _T("\\mysys"));
 
-	CServerPath hpnonstop2(_T("\\mysys.$myvol"), HPNONSTOP);
+	const CServerPath hpnonstop2(_T("\\mysys.$myvol"), HPNONSTOP);
 	CPPUNIT_ASSERT(hpnonstop2.GetPath() == _T("\\mysys.$myvol"));
 
-	CServerPath hpnonstop3(_T("\\mysys.$myvol.mysubvol"), HPNONSTOP);
+	const CServerPath hpnonstop3(_T("\\mysys.$myvol.mysubvol"), HPNONSTOP);
 	CPPUNIT_ASSERT(hpnonstop3.GetPath() == _T("\\mysys.$myvol.mysubvol"));
 
-	CServerPath dos_virtual1(_T("\\"));
+	const CServerPath dos_virtual1(_T("\\"));
 	CPPUNIT_ASSERT(dos_virtual1.GetPath() == _T("\\"));
 
-	CServerPath dos_virtual2(_T("\\foo"));
+	const CServerPath dos_virtual2(_T("\\foo"));
 	CPPUNIT_ASSERT(dos_virtual2.GetPath() == _T("\\foo"));
 
-	CServerPath dos_virtual3(_T("\\foo\\bar"));
+	const CServerPath dos_virtual3(_T("\\foo\\bar"));
 	CPPUNIT_ASSERT(dos_virtual3.GetPath() == _T("\\foo\\bar"));
 }
 
 void CServerPathTest::testHasParent()
 {
-	CServerPath unix1(_T("/"));
+	const CServerPath unix1(_T("/"));
 	CPPUNIT_ASSERT(!unix1.HasParent());
 
-	CServerPath unix2(_T("/foo"));
+	const CServerPath unix2(_T("/foo"));
 	CPPUNIT_ASSERT(unix2.HasParent());
 
-	CServerPath unix3(_T("/foo/bar"));
+	const CServerPath unix3(_T("/foo/bar"));
 	CPPUNIT_ASSERT(unix3.HasParent());
 
-	CServerPath vms1(_T("FOO:[BAR]"));
+	const CServerPath vms1(_T("FOO:[BAR]"));
 	CPPUNIT_ASSERT(!vms1.HasParent());
 
-	CServerPath vms2(_T("FOO:[BAR.TEST]"));
+	const CServerPath vms2(_T("FOO:[BAR.TEST]"));
 	CPPUNIT_ASSERT(vms2.HasParent());
 
-	CServerPath vms3(_T("FOO:[BAR^.TEST]"));
+	const CServerPath vms3(_T("FOO:[BAR^.TEST]"));
 	CPPUNIT_ASSERT(!vms3.HasParent());
 
-	CServerPath vms4(_T("FOO:[BAR^.TEST.SOMETHING]"));
+	const CServerPath vms4(_T("FOO:[BAR^.TEST.SOMETHING]"));
 	CPPUNIT_ASSERT(vms4.HasParent());
 
-	CServerPath dos1(_T("C:\\"));
+	const CServerPath dos1(_T("C:\\"));
 	CPPUNIT_ASSERT(!dos1.HasParent());
 
-	CServerPath dos2(_T("C:\\FOO"));
+	const CServerPath dos2(_T("C:\\FOO"));
 	CPPUNIT_ASSERT(dos2.HasParent());
 
-	CServerPath mvs1(_T("'FOO'"), MVS);
+	const CServerPath mvs1(_T("'FOO'"), MVS);
 	CPPUNIT_ASSERT(!mvs1.HasParent());
 
-	CServerPath mvs2(_T("'FOO.'"), MVS);
+	const CServerPath mvs2(_T("'FOO.'"), MVS);
 	CPPUNIT_ASSERT(!mvs2.HasParent());
 
-	CServerPath mvs3(_T("'FOO.BAR'"), MVS);
+	const CServerPath mvs3(_T("'FOO.BAR'"), MVS);
 	CPPUNIT_ASSERT(mvs3.HasParent());
 
-	CServerPath mvs4(_T("'FOO.BAR.'"), MVS);
+	const CServerPath mvs4(_T("'FOO.BAR.'"), MVS);
 	CPPUNIT_ASSERT(mvs4.HasParent());
 
-	CServerPath vxworks1(_T(":foo:"));
+	const CServerPath vxworks1(_T(":foo:"));
 	CPPUNIT_ASSERT(!vxworks1.HasParent());
 
-	CServerPath vxworks2(_T(":foo:bar"));
+	const CServerPath vxworks2(_T(":foo:bar"));
 	CPPUNIT_ASSERT(vxworks2.HasParent());
 
-	CServerPath vxworks3(_T(":foo:bar/test"));
+	const CServerPath vxworks3(_T(":foo:bar/test"));
 	CPPUNIT_ASSERT(vxworks3.HasParent());
 
 	// ZVM is same as Unix, only makes difference in directory
 	// listing parser.
-	CServerPath zvm1(_T("/"), ZVM);
+	const CServerPath zvm1(_T("/"), ZVM);
 	CPPUNIT_ASSERT(!zvm1.HasParent());
 
-	CServerPath zvm2(_T("/foo"), ZVM);
+	const CServerPath zvm2(_T("/foo"), ZVM);
 	CPPUNIT_ASSERT(zvm2.HasParent());
 
-	CServerPath zvm3(_T("/foo/bar"), ZVM);
+	const CServerPath zvm3(_T("/foo/bar"), ZVM);
 	CPPUNIT_ASSERT(zvm3.HasParent());
 
-	CServerPath hpnonstop1(_T("\\mysys"), HPNONSTOP);
+	const CServerPath hpnonstop1(_T("\\mysys"), HPNONSTOP);
 	CPPUNIT_ASSERT(!hpnonstop1.HasParent());
 
-	CServerPath hpnonstop2(_T("\\mysys.$myvol"), HPNONSTOP);
+	const CServerPath hpnonstop2(_T("\\mysys.$myvol"), HPNONSTOP);
 	CPPUNIT_ASSERT(hpnonstop2.HasParent());
 
-	CServerPath hpnonstop3(_T("\\mysys.$myvol.mysubvol"), HPNONSTOP);
+	const CServerPath hpnonstop3(_T("\\mysys.$myvol.mysubvol"), HPNONSTOP);
 	CPPUNIT_ASSERT(hpnonstop3.HasParent());
 
-	CServerPath dos_virtual1(_T("\\"));
+	const CServerPath dos_virtual1(_T("\\"));
 	CPPUNIT_ASSERT(!dos_virtual1.HasParent());
 
-	CServerPath dos_virtual2(_T("\\foo"));
+	const CServerPath dos_virtual2(_T("\\foo"));
 	CPPUNIT_ASSERT(dos_virtual2.HasParent());
 
-	CServerPath dos_virtual3(_T("\\foo\\bar"));
+	const CServerPath dos_virtual3(_T("\\foo\\bar"));
 	CPPUNIT_ASSERT(dos_virtual3.HasParent());
 }
 
 void CServerPathTest::testGetParent()
 {
-	CServerPath unix1(_T("/"));
-	CServerPath unix2(_T("/foo"));
-	CServerPath unix3(_T("/foo/bar"));
+	const CServerPath unix1(_T("/"));
+	const CServerPath unix2(_T("/foo"));
+	const CServerPath unix3(_T("/foo/bar"));
 	CPPUNIT_ASSERT(unix2.GetParent() == unix1);
 	CPPUNIT_ASSERT(unix3.GetParent() == unix2);
 
-	CServerPath vms1(_T("FOO:[BAR]"));
-	CServerPath vms2(_T("FOO:[BAR.TEST]"));
-	CServerPath vms3(_T("FOO:[BAR^.TEST]"));
-	CServerPath vms4(_T("FOO:[BAR^.TEST.SOMETHING]"));
+	const CServerPath vms1(_T("FOO:[BAR]"));
+	const CServerPath vms2(_T("FOO:[BAR.TEST]"));
+	const CServerPath vms3(_T("FOO:[BAR^.TEST]"));
+	const CServerPath vms4(_T("FOO:[BAR^.TEST.SOMETHING]"));
 	CPPUNIT_ASSERT(vms2.GetParent() == vms1);
 	CPPUNIT_ASSERT(vms4.GetParent() == vms3);
 
-	CServerPath dos1(_T("C:\\"));
-	CServerPath dos2(_T("C:\\FOO"));
+	const CServerPath dos1(_T("C:\\"));
+	const CServerPath dos2(_T("C:\\FOO"));
 	CPPUNIT_ASSERT(dos2.GetParent() == dos1);
 
-	CServerPath mvs1(_T("'FOO'"), MVS);
-	CServerPath mvs2(_T("'FOO.'"), MVS);
-	CServerPath mvs3(_T("'FOO.BAR'"), MVS);
-	CServerPath mvs4(_T("'FOO.BAR.'"), MVS);
+	const CServerPath mvs1(_T("'FOO'"), MVS);
+	const CServerPath mvs2(_T("'FOO.'"), MVS);
+	const CServerPath mvs3(_T("'FOO.BAR'"), MVS);
+	const CServerPath mvs4(_T("'FOO.BAR.'"), MVS);
 	CPPUNIT_ASSERT(mvs3.GetParent() == mvs2);
 	CPPUNIT_ASSERT(mvs4.GetParent() == mvs2);
 
-	CServerPath vxworks1(_T(":foo:"));
-	CServerPath vxworks2(_T(":foo:bar"));
-	CServerPath vxworks3(_T(":foo:bar/test"));
+	const CServerPath vxworks1(_T(":foo:"));
+	const CServerPath vxworks2(_T(":foo:bar"));
+	const CServerPath vxworks3(_T(":foo:bar/test"));
 	CPPUNIT_ASSERT(vxworks2.GetParent() == vxworks1);
 	CPPUNIT_ASSERT(vxworks3.GetParent() == vxworks2);
 
 	// ZVM is same as Unix, only makes difference in directory
 	// listing parser.
-	CServerPath zvm1(_T("/"), ZVM);
-	CServerPath zvm2(_T("/foo"), ZVM);
-	CServerPath zvm3(_T("/foo/bar"), ZVM);
+	const CServerPath zvm1(_T("/"), ZVM);
+	const CServerPath zvm2(_T("/foo"), ZVM);
+	const CServerPath zvm3(_T("/foo/bar"), ZVM);
 	CPPUNIT_ASSERT(zvm2.GetParent() == zvm1);
 	CPPUNIT_ASSERT(zvm3.GetParent() == zvm2);
 
-	CServerPath hpnonstop1(_T("\\mysys"), HPNONSTOP);
-	CServerPath hpnonstop2(_T("\\mysys.$myvol"), HPNONSTOP);
-	CServerPath hpnonstop3(_T("\\mysys.$myvol.mysubvol"), HPNONSTOP);
+	const CServerPath hpnonstop1(_T("\\mysys"), HPNONSTOP);
+	const CServerPath hpnonstop2(_T("\\mysys.$myvol"), HPNONSTOP);
+	const CServerPath hpnonstop3(_T("\\mysys.$myvol.mysubvol"), HPNONSTOP);
 	CPPUNIT_ASSERT(hpnonstop2.GetParent() == hpnonstop1);
 	CPPUNIT_ASSERT(hpnonstop3.GetParent() == hpnonstop2);
 
-	CServerPath dos_virtual1(_T("\\"));
-	CServerPath dos_virtual2(_T("\\foo"));
-	CServerPath dos_virtual3(_T("\\foo\\bar"));
+	const CServerPath dos_virtual1(_T("\\"));
+	const CServerPath dos_virtual2(_T("\\foo"));
+	const CServerPath dos_virtual3(_T("\\foo\\bar"));
 	CPPUNIT_ASSERT(dos_virtual2.GetParent() == dos_virtual1);
 	CPPUNIT_ASSERT(dos_virtual3.GetParent() == dos_virtual2);
 }
@@ -280,12 +282,16 @@ void CServerPathTest::testGetCommonParent()
 	const CServerPath mvs3(_T("'FOO.BAR'"), MVS);
 	const CServerPath mvs4(_T("'FOO.BAR.'"), MVS);
 	const CServerPath mvs5(_T("'BAR.'"), MVS);
+	const CServerPath mvs6(_T("'FOO.BAR.BAZ'"), MVS);
 	CPPUNIT_ASSERT(mvs1.GetCommonParent(mvs2) == CServerPath());
 	CPPUNIT_ASSERT(mvs3.GetCommonParent(mvs1) == CServerPath());
 	CPPUNIT_ASSERT(mvs4.GetCommonParent(mvs3) == mvs2);
+	CPPUNIT_ASSERT(mvs3.GetCommonParent(mvs4) == mvs2);
 	CPPUNIT_ASSERT(mvs3.GetCommonParent(mvs2) == mvs2);
 	CPPUNIT_ASSERT(mvs4.GetCommonParent(mvs2) == mvs2);
 	CPPUNIT_ASSERT(mvs5.GetCommonParent(mvs2) == CServerPath());
+	CPPUNIT_ASSERT(mvs6.GetCommonParent(mvs4) == mvs4);
+	CPPUNIT_ASSERT(mvs6.GetCommonParent(mvs3) == mvs2);
 
 	const CServerPath vxworks1(_T(":foo:"));
 	const CServerPath vxworks2(_T(":foo:bar"));
@@ -316,4 +322,51 @@ void CServerPathTest::testGetCommonParent()
 	const CServerPath dos_virtual3(_T("\\foo\\bar"));
 	CPPUNIT_ASSERT(dos_virtual2.GetCommonParent(dos_virtual3) == dos_virtual1);
 	CPPUNIT_ASSERT(dos_virtual1.GetCommonParent(dos_virtual1) == dos_virtual1);
+}
+
+void CServerPathTest::testFormatFilename()
+{
+	const CServerPath unix1(_T("/"));
+	const CServerPath unix2(_T("/foo"));
+	CPPUNIT_ASSERT(unix1.FormatFilename(_T("bar"), false) == _T("/bar"));
+	CPPUNIT_ASSERT(unix2.FormatFilename(_T("bar"), false) == _T("/foo/bar"));
+
+	const CServerPath vms1(_T("FOO:[BAR]"));
+	CPPUNIT_ASSERT(vms1.FormatFilename(_T("BAZ"), false) == _T("FOO:[BAR]BAZ"));
+
+	const CServerPath dos1(_T("C:\\"));
+	const CServerPath dos2(_T("C:\\foo"));
+	CPPUNIT_ASSERT(dos1.FormatFilename(_T("bar"), false) == _T("C:\\bar"));
+	CPPUNIT_ASSERT(dos2.FormatFilename(_T("bar"), false) == _T("C:\\foo\\bar"));
+
+	const CServerPath mvs1(_T("'FOO.BAR'"), MVS);
+	const CServerPath mvs2(_T("'FOO.BAR.'"), MVS);
+	CPPUNIT_ASSERT(mvs1.FormatFilename(_T("BAZ"), false) == _T("'FOO.BAR(BAZ)'"));
+	CPPUNIT_ASSERT(mvs2.FormatFilename(_T("BAZ"), false) == _T("'FOO.BAR.BAZ'"));
+
+	const CServerPath vxworks1(_T(":foo:"));
+	const CServerPath vxworks2(_T(":foo:bar"));
+	const CServerPath vxworks3(_T(":foo:bar/test"));
+	CPPUNIT_ASSERT(vxworks1.FormatFilename(_T("baz"), false) == _T(":foo:baz"));
+	CPPUNIT_ASSERT(vxworks2.FormatFilename(_T("baz"), false) == _T(":foo:bar/baz"));
+	CPPUNIT_ASSERT(vxworks3.FormatFilename(_T("baz"), false) == _T(":foo:bar/test/baz"));
+
+	// ZVM is same as Unix, only makes difference in directory
+	// listing parser.
+	const CServerPath zvm1(_T("/"));
+	const CServerPath zvm2(_T("/foo"));
+	CPPUNIT_ASSERT(zvm1.FormatFilename(_T("bar"), false) == _T("/bar"));
+	CPPUNIT_ASSERT(zvm2.FormatFilename(_T("bar"), false) == _T("/foo/bar"));
+
+	const CServerPath hpnonstop1(_T("\\mysys"), HPNONSTOP);
+	const CServerPath hpnonstop2(_T("\\mysys.$myvol"), HPNONSTOP);
+	const CServerPath hpnonstop3(_T("\\mysys.$myvol.mysubvol"), HPNONSTOP);
+	CPPUNIT_ASSERT(hpnonstop1.FormatFilename(_T("foo"), false) == _T("\\mysys.foo"));
+	CPPUNIT_ASSERT(hpnonstop2.FormatFilename(_T("foo"), false) == _T("\\mysys.$myvol.foo"));
+	CPPUNIT_ASSERT(hpnonstop3.FormatFilename(_T("foo"), false) == _T("\\mysys.$myvol.mysubvol.foo"));
+
+	const CServerPath dos_virtual1(_T("/"));
+	const CServerPath dos_virtual2(_T("/foo"));
+	CPPUNIT_ASSERT(dos_virtual1.FormatFilename(_T("bar"), false) == _T("/bar"));
+	CPPUNIT_ASSERT(dos_virtual2.FormatFilename(_T("bar"), false) == _T("/foo/bar"));
 }
