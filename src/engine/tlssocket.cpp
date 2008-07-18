bb3f67c28ce2918f220163754a5883f494c1ec9c
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2008-07-18 10:52:44 +0000

    Don't send two connection events, tiny bit of cleanup.

--- src/engine/tlssocket.cpp
+++ src/engine/tlssocket.cpp
@@ -254,12 +254,9 @@ ssize_t CTlsSocket::PullFunction(void* data, size_t len)
 				CSocketEvent evt(m_pSocketBackend->GetId(), CSocketEvent::close);
 				wxPostEvent(this, evt);
 			}
-
-			gnutls_transport_set_errno(m_session, EAGAIN);
-			return -1;
 		}
 
-		gnutls_transport_set_errno(m_session, 0);
+		gnutls_transport_set_errno(m_session, m_pSocketBackend->LastError());
 		return -1;
 	}
 
@@ -401,13 +398,6 @@ int CTlsSocket::Handshake(const CTlsSocket* pPrimarySocket /*=0*/)
 		res = VerifyCertificate();
 		if (res != FZ_REPLY_OK)
 			return res;
-
-		m_tlsState = conn;
-
-		CSocketEvent evt(GetId(), CSocketEvent::connection);
-		wxPostEvent(m_pEvtHandler, evt);
-		CheckResumeFailedReadWrite();
-		TriggerEvents();
 
 		if (m_shutdown_requested)
 		{
