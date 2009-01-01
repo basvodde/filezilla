741aaea1bcc76b78029c205b47da68201755fb94
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2009-01-01 11:46:11 +0000

    Happy new year.

--- src/interface/aboutdialog.cpp
+++ src/interface/aboutdialog.cpp
@@ -14,7 +14,7 @@ bool CAboutDialog::Create(wxWindow* parent)
 
 	XRCCTRL(*this, "ID_URL", wxHyperlinkCtrl)->SetLabel(_T("http://filezilla-project.org"));
 
-	XRCCTRL(*this, "ID_COPYRIGHT", wxStaticText)->SetLabel(_T("Copyright (C) 2004-2008  Tim Kosse"));
+	XRCCTRL(*this, "ID_COPYRIGHT", wxStaticText)->SetLabel(_T("Copyright (C) 2004-2009  Tim Kosse"));
 
 	wxString version = CBuildInfo::GetVersion();
 	if (CBuildInfo::GetBuildType() == _T("nightly"))
