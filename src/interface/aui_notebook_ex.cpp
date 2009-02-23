d5f2a68e908f1def56abca4540898be86f2829fa
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2009-02-23 15:51:42 +0000

    Aui tab art on dark themes

--- src/interface/aui_notebook_ex.cpp
+++ src/interface/aui_notebook_ex.cpp
@@ -1,6 +1,120 @@
 #include "FileZilla.h"
 #include <wx/aui/aui.h>
 #include "aui_notebook_ex.h"
+#include <wx/dcmirror.h>
+
+wxColor wxAuiStepColour(const wxColor& c, int ialpha);
+
+// Special DC to filter some calls.
+// We can (mis)use wxMirrorDC since it already
+// forwards all calls to another DC.
+//
+// We use this class to change the colours wxAuiTabArt uses
+// for drawing the tabs on dark themes.
+class CFilterDC : public wxMirrorDC
+{
+public:
+	CFilterDC(wxDC& dc, int type)
+		: wxMirrorDC(dc, false), m_original_dc(&dc), m_type(type)
+	{
+		m_gradient_called = 0;
+		m_rectangle_called = 0;
+	}
+
+	virtual void DoDrawRectangle(wxCoord x, wxCoord y, wxCoord width, wxCoord height)
+	{
+		if (!m_type)
+		{
+			m_original_dc->DrawRectangle(x, y, width, height);
+			return;
+		}
+		else if (m_type == 1)
+		{
+			m_rectangle_called++;
+
+			wxColour c = wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOWFRAME);
+			if (c.Red() + c.Green() + c.Blue() >= 384)
+				m_original_dc->DrawRectangle(x, y, width, height);
+			else
+			{
+				if (m_rectangle_called != 2)
+					m_original_dc->DrawRectangle(x, y, width, height);
+				else
+				{
+					wxColour new_init = wxAuiStepColour(c, 150);
+					wxColour new_dest = wxAuiStepColour(c, 100);
+					m_original_dc->GradientFillLinear(wxRect(x, y, width + 1, height), new_init, new_dest, wxNORTH);
+				}
+			}
+		}
+		else
+		{
+			wxColour c = wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOWFRAME);
+			if (c.Red() + c.Green() + c.Blue() < 384)
+				m_original_dc->SetBrush(wxBrush(wxAuiStepColour(c, 70)));
+			m_original_dc->DrawRectangle(x, y, width, height);
+		}
+	}
+	virtual void DoGradientFillLinear(const wxRect& rect, const wxColour& initialColour, const wxColour& destColour, wxDirection nDirection = wxEAST)
+	{
+		m_gradient_called++;
+
+		if (m_type == 1)
+		{
+			if (initialColour.Red() + initialColour.Green() + initialColour.Blue() >= 384)
+			{
+				// Light theme
+				m_original_dc->GradientFillLinear(rect, initialColour, destColour, nDirection);
+				return;
+			}
+
+			// Dark theme
+			wxColour new_init = wxAuiStepColour(initialColour, 100);
+			wxColour new_dest = wxAuiStepColour(initialColour, 120);
+			m_original_dc->GradientFillLinear(rect, new_init, new_dest, nDirection);
+		}
+		else if (!m_type)
+		{
+			if (destColour.Red() + destColour.Green() + destColour.Blue() >= 384)
+			{
+				// Light theme
+				m_original_dc->GradientFillLinear(rect, initialColour, destColour, nDirection);
+				return;
+			}
+
+			// Dark theme
+			if (m_gradient_called == 1)
+			{
+				wxColour new_init = wxAuiStepColour(destColour, 95);
+				wxColour new_dest = wxAuiStepColour(destColour, 65);
+				m_original_dc->GradientFillLinear(rect, new_init, new_dest, nDirection);
+			}
+			else
+			{
+				wxColour new_dest = wxAuiStepColour(destColour, 65);
+				m_original_dc->GradientFillLinear(rect, new_dest, new_dest, nDirection);
+			}
+		}
+		else if (m_type == 2)
+		{
+			wxColour c = wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOWFRAME);
+			if (c.Red() + c.Green() + c.Blue() >= 384)
+				m_original_dc->GradientFillLinear(rect, initialColour, destColour, nDirection);
+			else
+			{
+				wxColour new_init = wxAuiStepColour(c, 70);
+				wxColour new_dest = wxAuiStepColour(c, 100);
+				m_original_dc->GradientFillLinear(rect, new_init, new_dest, nDirection);
+			}
+		}
+	}
+
+protected:
+	int m_gradient_called;
+	int m_rectangle_called;
+	wxDC *m_original_dc;
+	const int m_type;
+};
 
 class wxAuiTabArtEx : public wxAuiDefaultTabArt
 {
@@ -51,8 +165,12 @@ public:
                          wxRect* out_button_rect,
                          int* x_extent)
 	{
+#ifndef __WXMAC__
+		m_base_colour = wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOWFRAME);
+#endif
 		if (!pane.active)
 		{
+			dc.SetTextForeground(wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOWTEXT));
 			if (m_pNotebook->Highlighted(m_pNotebook->GetPageIndex(pane.window)))
 			{
 				if (!m_fonts_initialized)
@@ -69,9 +187,15 @@ public:
 				m_normal_font = m_original_normal_font;
 		}
 
-		wxAuiDefaultTabArt::DrawTab(dc, wnd, pane, in_rect, close_button_state, out_tab_rect, out_button_rect, x_extent);
+		CFilterDC filter_dc(dc, pane.active ? 1 : 0);
+		wxAuiDefaultTabArt::DrawTab(*((wxDC*)&filter_dc), wnd, pane, in_rect, close_button_state, out_tab_rect, out_button_rect, x_extent);
 	}
 
+	virtual void DrawBackground(wxDC& dc, wxWindow* wnd, const wxRect& rect)
+	{
+		CFilterDC filter_dc(dc, 2);
+		wxAuiDefaultTabArt::DrawBackground(*((wxDC*)&filter_dc), wnd, rect);
+	}
 protected:
 	wxAuiNotebookEx* m_pNotebook;
 
