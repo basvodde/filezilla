bd8dcae1110b40137f70001acd7d3dbd28a345f2
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2009-10-14 14:00:43 +0000

    Fix action mismatch for default file exists action

--- src/include/notification.h
+++ src/include/notification.h
@@ -156,11 +156,11 @@ public:
 		ask,
 		overwrite,
 		overwriteNewer,	// Overwrite if source file is newer than target file
+		overwriteSize,	// Overwrite if source file is is different in size than target file
+		overwriteSizeOrNewer,	// Overwrite if source file is different in size or newer than target file
 		resume, // Overwrites if cannot be resumed
 		rename,
 		skip,
-		overwriteSize,	// Overwrite if source file is is different in size than target file
-		overwriteSizeOrNewer,	// Overwrite if source file is different in size or newer than target file
 
 		ACTION_COUNT
 	};
