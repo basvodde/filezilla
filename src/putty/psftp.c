34593334b263da53d63e836801e3a24f1cefdbd3
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2010-01-18 21:43:41 +0000

    PuTTY's exitcode handling is strange, using INT_MAX like that...

--- src/putty/psftp.c
+++ src/putty/psftp.c
@@ -3457,6 +3457,6 @@ void notify_remote_exit(void *frontend)
 {
     int exitcode = back->exitcode(backhandle);
 
-    if (exitcode > 0)
+    if (exitcode > 0 && exitcode != INT_MAX)
 	connection_fatal(frontend, "Connection closed by server with exitcode %d", exitcode);
 }
