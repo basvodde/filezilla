dc74040732f13f6690d161f2e065988e7ca17c4e
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2010-07-22 21:55:30 +0000

    * Fix for settings dir moved to options

--- src/interface/ipcmutex.cpp
+++ src/interface/ipcmutex.cpp
@@ -1,6 +1,6 @@
 #include <filezilla.h>
 #include "ipcmutex.h"
-#include "filezillaapp.h"
+#include "Options.h"
 
 #ifndef __WXMSW__
 #include <errno.h>
@@ -20,7 +20,7 @@ CInterProcessMutex::CInterProcessMutex(enum t_ipcMutexType mutexType, bool initi
 	if (!m_instanceCount)
 	{
 		// Open file only if this is the first instance
-		wxFileName fn(wxGetApp().GetSettingsDir(), _T("lockfile"));
+		wxFileName fn(COptions::Get()->GetOption(OPTION_DEFAULT_SETTINGSDIR), _T("lockfile"));
 		m_fd = open(fn.GetFullPath().mb_str(), O_CREAT | O_RDWR, 0644);
 	}
 	m_instanceCount++;
