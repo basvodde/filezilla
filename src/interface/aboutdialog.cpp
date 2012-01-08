370416ec0ccafa5b58c0204b9b4c99f65d120077
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2012-01-08 09:47:16 +0000

    Bump copyright years.

--- src/interface/aboutdialog.cpp
+++ src/interface/aboutdialog.cpp
@@ -20,7 +20,7 @@ bool CAboutDialog::Create(wxWindow* parent)
 
 	XRCCTRL(*this, "ID_URL", wxHyperlinkCtrl)->SetLabel(_T("http://filezilla-project.org"));
 
-	XRCCTRL(*this, "ID_COPYRIGHT", wxStaticText)->SetLabel(_T("Copyright (C) 2004-2011  Tim Kosse"));
+	XRCCTRL(*this, "ID_COPYRIGHT", wxStaticText)->SetLabel(_T("Copyright (C) 2004-2012  Tim Kosse"));
 
 	wxString version = CBuildInfo::GetVersion();
 	if (CBuildInfo::GetBuildType() == _T("nightly"))
